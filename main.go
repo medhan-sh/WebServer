bc2aac5c9ba30d52948df2d2c14f90d46417a589
medhan-sh <medshon@proton.me>
2026-08-01 02:20:17 +0530

    Add form handling and error response

--- main.go
+++ main.go
@@ -19,6 +19,18 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/form" {
+		http.Error(w, "404: not found", http.StatusNotFound)
+		return
+	}
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprint(w, "ParseForm err: %v", err)
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address =  %s\n", address)
 }
 
 func main() {
